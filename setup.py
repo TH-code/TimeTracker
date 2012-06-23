f7789e50c265a27082fe2aa889b44795e7e641a5
Thijs Jonkman <t.jonkman@gmail.com>
2012-06-24 00:11:49 +0200

    renamed to timelog, removed projekktor

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name='timetracker',
+    name='timelog',
     version='0.1',
     author="THijs",
     author_email="t.jonkman@gmail.com",
     description="",
     packages=find_packages(),
-    package_dir={'': os.sep.join(['src', 'timetracker'])},
+    package_dir={'': os.sep.join(['src', 'timelog'])},
     include_package_data=True,
     install_requires=[
       'bobo',
